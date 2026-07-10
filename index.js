29ab05964854d7b41b6ee2ec88bfdf0951a99f21
Peniel <penielkombe86@gmail.com>
2026-07-10 14:49:54 +0300

    feat: add button icons

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const buttonContainer = document.querySelector(".button-container")
-const ROWS = 4
+const ROWS = 5
 const COLUMNS = 4
 
+const calcBtnIcons = [
+  "C",   "⌫",  "%",  "÷",
+  "7",   "8",   "9",  "×",
+  "4",   "5",   "6",  "-",
+  "1",   "2",   "3",  "+",
+  "0",   ".",   "+/-", "="
+];
+
 function generateCalcBtns() {
-    for (let i = 0;i < ROWS * COLUMNS; i++) {
+    for (let i = 0;i < calcBtnIcons.length; i++) {
         let button = document.createElement("button")
+        let span = document.createElement("span")
+        
+        span.textContent = calcBtnIcons[i]
+        span.classList.add("btn-icon")
+
+        button.appendChild(span)
         buttonContainer.appendChild(button)
     }
 }
