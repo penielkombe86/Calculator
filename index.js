c33f8f2d69543fa839dce4f017f3d8e192a20f7a
Peniel <penielkombe86@gmail.com>
2026-07-11 13:24:39 +0300

    add ids to the calculator buttons

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const buttonContainer = document.querySelector(".button-container")
-const ROWS = 5
-const COLUMNS = 4
 
-const calcBtnIcons = [
-  "C",   "⌫",  "%",  "÷",
-  "7",   "8",   "9",  "×",
-  "4",   "5",   "6",  "-",
-  "1",   "2",   "3",  "+",
-  "0",   ".",   "+/-", "="
-];
+const calcBtnObject = [
+    {id: "clear", icon: "C"},
+    {id: "delete", icon: "⌫"},
+    {id: "percent", icon: "%"},
+    {id: "divide", icon: "÷"},
+    {id: "seven", icon: "7"},
+    {id: "eight", icon: "8"},
+    {id: "nine", icon: "9"},
+    {id: "multiply", icon: "×"},
+    {id: "four", icon: "4"},
+    {id: "five", icon: "5"},
+    {id: "six", icon: "6"},
+    {id: "minus", icon: "-"},
+    {id: "one", icon: "1"},
+    {id: "two", icon: "2"},
+    {id: "three", icon: "3"},
+    {id: "plus", icon: "+"},
+    {id: "one", icon: "1"},
+    {id: "zero", icon: "0"},
+    {id: "decimal", icon: "+/-"},
+    {id: "equal", icon: "="},
+]
+
 
 function generateCalcBtns() {
-    for (let i = 0;i < calcBtnIcons.length; i++) {
+    for (let i = 0;i < calcBtnObject.length; i++) {
         let button = document.createElement("button")
         let span = document.createElement("span")
         
-        span.textContent = calcBtnIcons[i]
+        span.textContent = calcBtnObject[i].icon
         span.classList.add("btn-icon")
 
         button.appendChild(span)
+        button.id = calcBtnObject[i].id
         buttonContainer.appendChild(button)
     }
 }
 
 generateCalcBtns()
+
+//buttonContainer.addEventListener("click")
