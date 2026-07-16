9e01b402c263f059010a666f8bc2375171e7ebc5
Peniel <penielkombe86@gmail.com>
2026-07-16 12:34:51 +0300

    feat add logic to handle the calculation and update the display

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+// logic behind updating the screen:
+// - keep track of the first No and second No and operator
+// - 
+
 const buttonContainer = document.querySelector(".button-container")
+let screenValue = document.querySelector(".screen-value")
+
+let firstNumber = ""
+let  operator = ""
+let  secondNumber = ""
+
+screenValue.textContent = 0
 
 const calcBtnObject = [
     {id: "clear", icon: "C"},
     {id: "delete", icon: "⌫"},
-    {id: "percent", icon: "%"},
+    {id: "percent", icon: "%"}, 
     {id: "divide", icon: "÷"},
     {id: "seven", icon: "7"},
     {id: "eight", icon: "8"},
     {id: "nine", icon: "9"},
-    {id: "multiply", icon: "×"},
+    {id: "multiply", icon: "x"},
     {id: "four", icon: "4"},
     {id: "five", icon: "5"},
     {id: "six", icon: "6"},
@@ -17,9 +28,9 @@ const calcBtnObject = [
     {id: "two", icon: "2"},
     {id: "three", icon: "3"},
     {id: "plus", icon: "+"},
-    {id: "one", icon: "1"},
     {id: "zero", icon: "0"},
-    {id: "decimal", icon: "+/-"},
+    {id: "decimal", icon: "."},
+    {id: "plus-minus", icon: "+/-"},
     {id: "equal", icon: "="},
 ]
 
@@ -34,10 +45,84 @@ function generateCalcBtns() {
 
         button.appendChild(span)
         button.id = calcBtnObject[i].id
+        button.classList.add("calc-btn")
         buttonContainer.appendChild(button)
+        // set the dataset of the buttons to the button icon for easier tracking
+        button.dataset.val= calcBtnObject[i].icon
     }
 }
 
 generateCalcBtns()
 
-//buttonContainer.addEventListener("click")
+function handleNumberInput(value) {
+    if (!isNaN(value) || value === ".") { // check if the value clicked is a number or has decimal places 
+        if (!operator) { // check if the operator is empty
+            firstNumber += value
+            screenValue.textContent = firstNumber
+        } else {
+            secondNumber += value
+            screenValue.textContent = secondNumber
+        }
+    }
+}
+
+function handleOperatorInput(value) {
+    if (!operator) { // check if the operator is empty
+        if (value === "x" || value === "-" || value === "÷" || value === "+") {
+            operator = value
+            screenValue.textContent = operator
+        }
+    }  
+}
+
+function operation() {
+    let result 
+    if (firstNumber && operator && secondNumber) {
+        switch (operator) {
+            case "x" :
+                result = Number(firstNumber) * Number(secondNumber)
+                break
+            case "+":
+                result = Number(firstNumber) + Number(secondNumber)
+                break
+            case "-":
+                result = Number(firstNumber) - Number(secondNumber)
+                break
+            case "÷":
+                result = Number(firstNumber) / Number(secondNumber)
+                break
+        }
+    }
+    screenValue.textContent = result
+}
+
+function resetCalculator() {
+    firstNumber = ""
+    secondNumber = ""
+    operator = ""
+    screenValue.textContent = "0"
+
+}
+
+function updateDisplay(event) {
+    const clickedButton = event.target.closest("button")
+
+    if (!clickedButton) return 
+
+    const value = clickedButton.dataset.val
+
+    handleNumberInput(value)
+    handleOperatorInput(value)
+    
+    if (value === "=") {
+        operation()
+    }
+    if (value === "C") {
+        resetCalculator()
+    }
+}
+
+buttonContainer.addEventListener("click", (event) =>{
+    updateDisplay(event)
+})
+
