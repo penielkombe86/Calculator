ce5ad822a2662cd2a0747952a6d02dc2f196234b
Peniel <penielkombe86@gmail.com>
2026-07-24 21:15:23 +0300

    feat: implement state driven ui to separate logic from ui

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 const buttonContainer = document.querySelector(".button-container")
 let screenValue = document.querySelector(".screen-value")
 
-let firstNumber = ""
-let  operator = ""
-let  secondNumber = ""
-let display = ""
+let state = {
+    firstNumber: "",
+    operator: "",
+    secondNumber: ""
+}
 
 screenValue.textContent = 0
 
@@ -55,96 +56,98 @@ function generateCalcBtns() {
 
 generateCalcBtns()
 
-function handleNumberInput(value) {
-    if (!isNaN(value) || value === ".") { // check if the value clicked is a number or has decimal places 
-        if (!operator) { // check if the operator is empty
-            firstNumber += value
-            display += value
-            screenValue.textContent = display
-        } else {
-            secondNumber += value
-            display += value
-            screenValue.textContent = display
-        }
+function render() {
+    // render the states on the screen in real time
+    let display = state.firstNumber + state.operator + state.secondNumber
+    if (state.firstNumber === "" && state.operator === "" && state.secondNumber === "") {
+        screenValue.textContent = 0
+        return 
     }
+    screenValue.textContent = display 
 }
 
-function handleOperatorInput(value) {
-    if (!operator) { // check if the operator is empty
-        if (value === "x" || value === "-" || value === "÷" || value === "+") {
-            operator = value
-            display += value
-            screenValue.textContent = display
-        }
-    }  
+function handleNegativeNumbers(value) {
+    // create new negative numbers when the user click the minus symbol
+    if (!state.firstNumber) { 
+        state.firstNumber = "-" // start a new negative number
+    }
+    else if (state.firstNumber && !state.operator) {
+        state.operator = "-" // 
+    }
+    else if (!state.secondNumber  && state.operator) {
+        state.secondNumber = "-" // start a new negative number
+    }
+    render()
+}
+
+function handleNumberInput(value) {
+    // simply appends new numbers to the firstNumber state
+    if (!state.operator) {
+        state.firstNumber += value
+    }
+    else {
+        state.secondNumber +=  value
+    }
+    render()
+}
+
+function handleOperatorInput(value){
+    if (!state.operator && !state.secondNumber) {
+        state.operator = value
+    }
+    render()
 }
 
 function operation() {
     let result 
-    if (firstNumber && operator && secondNumber) {
-        switch (operator) {
+    if (state.firstNumber && state.operator && state.secondNumber) {
+        switch (state.operator) {
             case "x" :
-                result = Number(firstNumber) * Number(secondNumber)
+                result = Number(state.firstNumber) * Number(state.secondNumber)
                 break
             case "+":
-                result = Number(firstNumber) + Number(secondNumber)
+                result = Number(state.firstNumber) + Number(state.secondNumber)
                 break
             case "-":
-                result = Number(firstNumber) - Number(secondNumber)
+                result = Number(state.firstNumber) - Number(state.secondNumber)
                 break
             case "÷":
-                result = Number(firstNumber) / Number(secondNumber)
+                result = Number(state.firstNumber) / Number(state.secondNumber)
                 break
         }
     }
-    display = result
-    screenValue.textContent = display
-    // reset the variable after the result is displayed
-    firstNumber = result?.toString() // used optional chaining to prevent crashing when empty values are encountered
-    operator = ""
-    secondNumber = ""
-    display = firstNumber
-    screenValue.textContent = display
+    return result 
 }
-function backspace() {
-    if (display === "" || display === 0) return 
-    
-    display = display.slice(0, -1)
-    screenValue.textContent = display // immediately update the screen when user clicks the backspace
 
-    // update the firstNumber, secondNumber and operator
-    if (operator !== "") { // check if operator is present
-        secondNumber = secondNumber.slice(0, -1)
+function backSpace() { 
+    if (!state.secondNumber && !state.operator) { // if operator and second number are empty
+        state.firstNumber = state.firstNumber.slice(0, -1)
     }
-    else if (operator !== "" && secondNumber === "") { // check if operator is present and secondNumber is empty
-        operator = ""
+    else if (!state.secondNumber && state.operator) { // if second number is empty and operator has a value
+        state.operator = ""
     }
     else {
-        firstNumber = firstNumber.slice(0, -1)
+        state.secondNumber = state.secondNumber.slice(0, -1)
     }
+    render()
 } 
 
-function resetCalculator() {
-    firstNumber = ""
-    secondNumber = ""
-    operator = ""
-    display = ""
-    screenValue.textContent = "0"
-
+function resetCalculator() { // clears the states
+    state.firstNumber = ""
+    state.operator = ""
+    state.secondNumber = ""
+    render()
 }
 
-function handlePercentages(value) {
-    // validations
-    if (!operator) {
-        firstNumber = firstNumber / 100
-        display += value
-        screenValue.textContent = display
+function handlePercentages(value) { // converts numbers to percentages
+    // know what number we are dealing with
+    if (!state.operator) {
+        state.firstNumber += "%"
     }
     else {
-        secondNumber = secondNumber / 100
-        display += value
-        screenValue.textContent = display
+        state.secondNumber += "%"
     }
+    render()
 }
 
 function updateDisplay(event) {
@@ -154,26 +157,40 @@ function updateDisplay(event) {
 
     const value = clickedButton.dataset.val
 
-    handleNumberInput(value)
-    handleOperatorInput(value)
+    // check negative number entered
+    if (value === "-") {
+        handleNegativeNumbers(value)
+    }
+    // check positive numbers and decimals
+    else if (!isNaN(value) || value === ".") {
+        handleNumberInput(value)
+    }
+    // check operator
+    else if (value === "x" || value === "+" || value === "÷") {
+        handleOperatorInput(value)
+    }
+    else {
+        switch (value) {
+            case "C":
+                resetCalculator()
+                break
+            case "⌫":
+                backSpace()
+                break
+            case "=":
+                result = operation()
+                if (result !== undefined) {
+                    screenValue.textContent = result
 
-    switch (value) {
-        case "C":
-            resetCalculator()
-            break
-        case "⌫":
-            backspace()
-            break
-        case "=":
-            operation()
-            break
-        case "%":
-            handlePercentages(value)
-            break
+                    state.firstNumber = result.toString()
+                    state.operator = ""
+                    state.secondNumber = ""
+                }
+                break
+        }
     }
 }
 
 buttonContainer.addEventListener("click", (event) =>{
     updateDisplay(event)
-})
-
+})   
